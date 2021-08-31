c49459e6f6e399cd94e5bbcf47dabc086fd8fd92
Alex Araujo <alexarajunior@gmail.com>
2021-08-30 23:21:34 -0300

    btnEditarExcluir

--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -1,30 +1,44 @@
-import { DateTimeFormatPipePipe } from './helps/DateTimeFormatPipe.pipe';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
+import { EventoService } from './services/evento.service';
+
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { DateTimeFormatPipePipe } from './helps/DateTimeFormatPipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-      EventosComponent,
-      NavComponent,
-      DateTimeFormatPipePipe
+    EventosComponent,
+    NavComponent,
+    DateTimeFormatPipePipe,
    ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    EventoService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
